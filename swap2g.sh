5c4035218c2dab2a85c114a5370557037e996e37
Marcos G <warcosyt@gmail.com>
2021-11-27 12:44:44 +0100

    Create swap2g.sh

---
+++ swap2g.sh
@@ -0,0 +1,6 @@
+sudo fallocate -l 2G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo "/swapfile swap swap defaults 0 0" | sudo tee -a /etc/fstab
+sudo swapon --show
